7510284e8fd7686fa92575f4e400786b31d1b7a6
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2021-12-18 08:47:54 -0300

    :up: informações pessoais

--- src/components/About/AboutItem.tsx
+++ src/components/About/AboutItem.tsx
@@ -7,21 +7,27 @@ export default function AboutItem() {
         <h1>Conheça minha história</h1>
         <div>
           <p>
-            <span className="purple">.sobre</span>
+            <span className="purple">.about</span>
             <span className="unicode"> {'\u007B'}</span> <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-            perspiciatis, nesciunt illum tenetur libero asperiores accusamus,
-            odit odit odit odit officiis consequuntur dolorum suscipit aut?
-            facilis facilis ipsa ullam tempora facere ut saepe minima
-            repudiandae vero, deleniti consequuntur doloremque voluptate eveniet
-            similique rem eligendi id aut? Quaerat maiores error aliquid quidem
-            quis numquam accusantium quibusdam officiis nam exercitationem,
-            temporibus beatae voluptatum itaque molestias doloremque
-            consectetur, eaque nulla suscipit. Rerum explicabo voluptates
-            molestiae? Quis, tempora! Totam culpa nulla distinctio accusamus
-            aliquid aliquam esse! Expedita, eum consequatur at omnis beatae,
-            earum ducimus aliquam debitis explicabo, sequi est. Eveniet unde sed
-            vero? Doloribus. <br />
+            Uma pessoa apaixonada pelo o conhecimento, inovação, criação e
+            tecnologia, desde de criança. Busquei na graduação de tecnologia
+            aprimorar esse conhecimento. Me apaixonei pelo o desenvolvimento
+            Front-End. Na faculdade surgiu o interesse irresistível
+            desenvolvimento, sendo assim à criação de site, aplicativos,
+            padronização de bancos de dados, organização de projetos. A
+            faculdade me ensinou de uma forma a ter uma habilidade de testa,
+            acerta, errar e aprender com isso, e nunca desistir. <br />
+            <span className="unicode">{'\u007D'}</span> <br />
+            <span className="purple">.person</span>
+            <span className="unicode"> {'\u007B'}</span> <br />
+            Acredito que um dos meus maiores ponto forte seja a forma de
+            trabalhar alegre, comunicativo, e a forma interessante que tenho em
+            aprender novas formas de trabalho. Proativo, procurando novas fontes
+            de adquirir conhecimento e experiência necessárias para se tornar um
+            bom profissional. Formado em Sistemas de Informações, tento buscar
+            forma de exercer de forma satisfatória o melhor trabalho possível.
+            Comprometimento pelo o trabalho, procurando fazer tudo com
+            excelência <br />
             <span className="unicode">{'\u007D'}</span>
           </p>
         </div>
