4bfadeec39aec32899cb23db458b049987a6340c
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2021-12-17 21:07:21 -0300

    :rocket: primeira parte da integração com o CMS da API do Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
 import { HomeContainer } from '../styles/HomeStyles';
 import Profiency from '../components/Profiency';
 import Header from '../components/Header';
@@ -7,8 +9,22 @@ import FinishProjects from '../components/FinishProjects';
 import Know from '../components/Know';
 import FormContact from '../components/FormContact';
 import Footer from '../components/Footer';
+import { getPrismicClient } from '../services/prismic';
 
-export default function Home() {
+interface InterfaceProject {
+  slug: string;
+  title: string;
+  type: string;
+  description: string;
+  link: string;
+  thumbnail: string;
+}
+
+interface HomeProps {
+  projects: InterfaceProject[];
+}
+
+export default function Home({ projects }: HomeProps) {
   return (
     <HomeContainer>
       <Header />
@@ -17,7 +33,7 @@ export default function Home() {
         <HomeHero />
         <About />
         <Profiency />
-        <FinishProjects />
+        <FinishProjects projects={projects} />
         <Know />
         <FormContact />
       </main>
@@ -26,3 +42,28 @@ export default function Home() {
     </HomeContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const projectResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'project')],
+    { orderings: '[document.fisrt_publication_date desc]' }
+  );
+
+  const projects = projectResponse.results.map(project => ({
+    slug: project.uid,
+    title: project.data.title,
+    type: project.data.type,
+    description: project.data.description,
+    link: project.data.link.url,
+    thumbnail: project.data.thumbnail.url
+  }));
+
+  return {
+    props: {
+      projects
+    },
+    revalidate: 172800
+  };
+};
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,37 +1,63 @@
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../../services/prismic';
 import Header from '../../components/Header';
 import ProjectsItems from '../../components/ProjectsItems';
 import { ProjectContainer } from '../../styles/ProjectsStyles';
 
-export default function Projects() {
+interface InterfaceProject {
+  slug: string;
+  title: string;
+  type: string;
+  description: string;
+  link: string;
+  thumbnail: string;
+}
+
+interface ProjectProps {
+  projects: InterfaceProject[];
+}
+
+export default function Projects({ projects }: ProjectProps) {
   return (
     <ProjectContainer>
       <Header />
       <main className="container">
-        <ProjectsItems
-          title="Projeto 01"
-          type="Website"
-          slug="teste"
-          imgUrl="https://user-images.githubusercontent.com/68359459/128270251-f4050d70-c53d-4c98-9ef3-73b7c52d6206.png"
-        />
-        <ProjectsItems
-          title="Projeto 01"
-          type="Website"
-          slug="teste"
-          imgUrl="https://user-images.githubusercontent.com/68359459/128270251-f4050d70-c53d-4c98-9ef3-73b7c52d6206.png"
-        />
-        <ProjectsItems
-          title="Projeto 01"
-          type="Website"
-          slug="teste"
-          imgUrl="https://user-images.githubusercontent.com/68359459/128270251-f4050d70-c53d-4c98-9ef3-73b7c52d6206.png"
-        />
-        <ProjectsItems
-          title="Projeto 01"
-          type="Website"
-          slug="teste"
-          imgUrl="https://user-images.githubusercontent.com/68359459/128270251-f4050d70-c53d-4c98-9ef3-73b7c52d6206.png"
-        />
+        {projects.map(project => (
+          <ProjectsItems
+            key={project.slug}
+            title={project.title}
+            type={project.type}
+            slug={project.slug}
+            imgUrl={project.thumbnail}
+          />
+        ))}
       </main>
     </ProjectContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const projectResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'project')],
+    { orderings: '[document.fisrt_publication_date desc]' }
+  );
+
+  const projects = projectResponse.results.map(project => ({
+    slug: project.uid,
+    title: project.data.title,
+    type: project.data.type,
+    description: project.data.description,
+    link: project.data.link.url,
+    thumbnail: project.data.thumbnail.url
+  }));
+
+  return {
+    props: {
+      projects
+    },
+    revalidate: 172800
+  };
+};
