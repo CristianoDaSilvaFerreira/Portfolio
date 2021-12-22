8e35a74357127e71e4979e42ba81316cc98628a6
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2021-12-22 20:40:00 -0300

    :up: alguns ajustes

--- src/components/Profiency/styles.ts
+++ src/components/Profiency/styles.ts
@@ -34,6 +34,7 @@ export const ItemContainer = styled.div`
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
+    max-width: 19rem;
 
     transition: 0.5s;
 
