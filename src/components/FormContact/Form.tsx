c6d27bab9d239fb21460166cb4837f1c600135e0
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2021-12-22 20:09:30 -0300

    :rocket: implementação do email atraves do notemails para serviço de email do Gmail

--- src/components/FormContact/Form.tsx
+++ src/components/FormContact/Form.tsx
@@ -1,12 +1,56 @@
+import { useState } from 'react';
+import { sendContactMail } from '../../services/sendMail';
 import { FormContainer, Input, TextArea } from './styles';
 
 export default function Form() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+      await sendContactMail(nome, email, mensagem);
+      setNome('');
+      setEmail('');
+      setMensagem('');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <FormContainer>
-      <Input placeholder="Nome" type="text" required />
-      <Input placeholder="E-mail" type="email" required />
-      <TextArea placeholder="Mensangem" required />
-      <button type="submit">ENVIAR</button>
+    <FormContainer
+      data-aos="fade-up"
+      data-aos-anchor-placement="center-bottom"
+      onSubmit={handleSubmit}
+    >
+      <Input
+        placeholder="Nome"
+        type="text"
+        value={nome}
+        onChange={({ target }) => setNome(target.value)}
+      />
+      <Input
+        placeholder="E-mail"
+        type="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <TextArea
+        placeholder="Mensangem"
+        value={mensagem}
+        onChange={({ target }) => setMensagem(target.value)}
+      />
+      <button type="submit" disabled={loading}>
+        ENVIAR
+      </button>
     </FormContainer>
   );
 }
