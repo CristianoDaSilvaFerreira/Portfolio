e3ec8756354d3793cc40c9a040a14e5bd20b11da
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2021-12-22 21:06:11 -0300

    :up: atualização de alguns detalhes

--- src/components/Profiency/index.tsx
+++ src/components/Profiency/index.tsx
@@ -9,28 +9,30 @@ function Profiency() {
 
       <section data-aos="fade-up" data-aos-anchor-placement="top-bottom">
         <ProfiencyItem
-          year="2020"
+          year="Julho 2020"
           title="Graduação"
           description="Bacharel em Sistema de Informações"
           institution="Estácio de Sá"
         />
         <ProfiencyItem
-          year="2020"
-          title="Graduação"
-          description="Bacharel em Sistema de Informações"
-          institution="Estácio de Sá"
+          year="Fevereiro 2021"
+          title="HTML Web Developer"
+          description="Curso de aprefeiçoamento através do Bootcamp"
+          institution="Digital Innovation One"
+          hour="27 horas"
         />
         <ProfiencyItem
-          year="2020"
-          title="Graduação"
-          description="Bacharel em Sistema de Informações"
-          institution="Estácio de Sá"
+          year="Maio 2021"
+          title="Desenvolvimento Web"
+          description="Curso online de Desenvolvimento Web"
+          institution="Udemy - Jorge Sant'Ana, Jamilto Damasceno e Udemy"
+          hour="109 horas"
         />
         <ProfiencyItem
-          year="2020"
-          title="Graduação"
-          description="Bacharel em Sistema de Informações"
-          institution="Estácio de Sá"
+          year="Maio 2021"
+          title="JavaScript"
+          description="Curso online de JavaScript"
+          institution="Hcode e Udemy"
         />
       </section>
     </Container>
